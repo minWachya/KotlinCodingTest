9d304af45fa277480243d70eb00f36e1dabc6113
minWachya <61674991+minWachya@users.noreply.github.com>
2025-05-19 13:52:31 +0900

    [level 2] Title: 피로도, Time: 4.41 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.kt
+++ 프로그래머스/2/87946. 피로도/피로도.kt
@@ -1,24 +1,26 @@
 class Solution {
+    private var answer = 0
+
     fun solution(k: Int, dungeons: Array<IntArray>): Int {
-        var answer = 0
-        
-        // 던전 탐험
+        val visited = BooleanArray(dungeons.size) { false }
+        findAnswer(k, dungeons, visited, 0)
+        return answer
+    }
+
+    fun findAnswer(
+        k: Int, 
+        dungeons: Array<IntArray>, 
+        visited: BooleanArray, 
+        count: Int) {
         for (i in dungeons.indices) {
-            // 탐험할 던전
-            var dungeon = dungeons[i]
-            // 던전이 탐험 가능하면 입장
-            if (k >= dungeon[0]) {
-                // 피로도 소모, 해당 던전 제외한 배열로 재탐색
-                var temp = solution(
-                    k - dungeon[1], 
-                    dungeons.sliceArray(0..i-1) + 
-                        dungeons.sliceArray(i+1..dungeons.lastIndex))
-                // 이후 탐색한 결과와 이전에 탐색한 결과 중 큰 값 저장
-                if (temp + 1 > answer) answer = temp + 1
-                if (answer == dungeons.count()) return answer
+            // 방문한 적 없고 방문할 체력이 되면 ㄱㄱ
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true
+                findAnswer(k - dungeons[i][1], dungeons, visited, count + 1)
+                visited[i] = false
             }
         }
-        
-        return answer
+
+        if (count > answer) answer = count
     }
 }
