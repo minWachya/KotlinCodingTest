82cd4666c02e869d5d34a77ac1ec62f5fc61856b
minWachya <61674991+minWachya@users.noreply.github.com>
2025-05-19 13:28:07 +0900

    [level 2] Title: 피로도, Time: 41.12 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.kt
+++ 프로그래머스/2/87946. 피로도/피로도.kt
@@ -2,13 +2,18 @@ class Solution {
     fun solution(k: Int, dungeons: Array<IntArray>): Int {
         var answer = 0
         
+        // 던전 탐험
         for (i in dungeons.indices) {
+            // 탐험할 던전
             var dungeon = dungeons[i]
+            // 던전이 탐험 가능하면 입장
             if (k >= dungeon[0]) {
+                // 피로도 소모, 해당 던전 제외한 배열로 재탐색
                 var temp = solution(
                     k - dungeon[1], 
                     dungeons.sliceArray(0..i-1) + 
                         dungeons.sliceArray(i+1..dungeons.lastIndex))
+                // 이후 탐색한 결과와 이전에 탐색한 결과 중 큰 값 저장
                 if (temp + 1 > answer) answer = temp + 1
                 if (answer == dungeons.count()) return answer
             }
