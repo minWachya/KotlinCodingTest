f7a7140ce721f329de542ffb0c7681dc4aee54cb
minWachya <61674991+minWachya@users.noreply.github.com>
2025-05-20 13:25:01 +0900

    [level 2] Title: 튜플, Time: 111.38 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.kt
+++ 프로그래머스/2/64065. 튜플/튜플.kt
@@ -1,37 +1,9 @@
 class Solution {
-    fun solution(s: String): IntArray {
-        var answer = intArrayOf()
-        var tuples = arrayOf<IntArray>()
-        
-        var tempArr = intArrayOf()
-        var i = 0
-        while(i < s.length) { 
-            if(s[i] == '}') {
-                tuples += tempArr
-                tempArr = intArrayOf()
-            }
-            else if(s[i].isDigit()) {
-                var tempNum = ""
-                while(s[i].isDigit()) {
-                    tempNum += s[i]
-                    i++
-                }
-                i--
-                tempArr += tempNum.toInt()
-            }
-            i++
-        }
-        
-        
-        tuples.sortedWith(compareBy({it.size})).forEach{
-            for(c in it) {
-                if(c !in answer) { 
-                    answer += c
-                    break
-                }
-            }
-        }
-        
-        return answer
-    }
+    fun solution(s: String): IntArray = s.substring(2..s.lastIndex-2)
+        .split("},{")
+        .asSequence()
+        .map{ it.split(",").map(String::toInt) }
+        .sortedBy{ it.size }
+        .fold(setOf<Int>()) { acc, list -> acc.union(list) }
+        .toIntArray()
 }
