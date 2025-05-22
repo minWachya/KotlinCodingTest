bb5babb994901a9a03b39a49900d11aabbf4c7f8
minWachya <61674991+minWachya@users.noreply.github.com>
2025-05-22 13:51:10 +0900

    [level 2] Title: 프로세스, Time: 25.36 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.kt
+++ 프로그래머스/2/42587. 프로세스/프로세스.kt
@@ -2,9 +2,9 @@ import java.util.*
 
 class Solution {
     fun solution(priorities: IntArray, location: Int): Int {
-        var answer = -1
         var queue: Queue<Int> = LinkedList()
         var queueIndex: Queue<Int> = LinkedList()
+        
         for(i in 0 until priorities.size) {
             queue.offer(priorities[i])
             queueIndex.offer(i)
@@ -12,13 +12,13 @@ class Solution {
         val sortPri = priorities.sortedDescending()
         var index = 0
         
-        while(answer == -1) {
+        while(true) {
             var p = queue.poll()
             var pi = queueIndex.poll()
             
             if(p == sortPri[index]) {
                 index++
-                if(pi == location) answer = index
+                if(pi == location) return index
             }
             else {
                 queue.offer(p)
@@ -26,7 +26,5 @@ class Solution {
             }
             
         }
-        
-        return answer
     }
 }
