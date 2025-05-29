b1b888f0c96048fb3c02725ae02107e221c88b4b
minWachya <61674991+minWachya@users.noreply.github.com>
2025-05-29 14:54:40 +0900

    [level 2] Title: 타겟 넘버, Time: 120.61 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
@@ -1,9 +1,7 @@
 class Solution {
-    fun solution(numbers: IntArray, target: Int): Int = dfs(numbers, target, 0, 0)
-    
-    fun dfs(numbers: IntArray, target: Int, i: Int, sum: Int): Int {
-        if(numbers.size == i) return if(sum == target) 1 else 0
-        
-        return dfs(numbers, target, i+1, sum+numbers[i]) + dfs(numbers, target, i+1, sum-numbers[i])
-    }
+    fun solution(numbers: IntArray, target: Int): Int = numbers
+        .fold(listOf(0)) { list, num ->
+            list.map { it + num } + list.map { it - num }
+        }
+        .count { it == target }
 }
