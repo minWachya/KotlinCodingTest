efb8dc581e63e871ea6c472a656e63a2f0b58032
minWachya <61674991+minWachya@users.noreply.github.com>
2025-05-30 13:43:12 +0900

    [level 1] Title: 옹알이 (2), Time: 14.94 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.kt
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.kt
@@ -1,20 +1,7 @@
 class Solution {
-    fun solution(babbling: Array<String>): Int {
-        var answer: Int = 0
-        val babblingArr = arrayOf<String>("aya", "ye", "woo", "ma")
-        
-        for(word in babbling) {
-            var temp = word
-            
-            for(b in babblingArr) {
-                temp = temp.replace(b, "X")
-                if(temp.contains("XX")) break
-                else temp = temp.replace("X", " ")
-            }
-            temp = temp.replace(" ", "")
-            if(temp.length == 0) answer++
-        }
-        
-        return answer
-    }
+    fun solution(babbling: Array<String>): Int = babbling
+            .map{ it.replace("ayaaya|yeye|woowoo|mama".toRegex(),"*") }
+            .map{ it.replace("aya|ye|woo|ma".toRegex(), "") }
+            .filter { it == "" }
+            .size
 }
