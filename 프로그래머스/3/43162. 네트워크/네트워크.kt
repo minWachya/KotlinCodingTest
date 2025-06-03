97113c86292251b6663a4079e7f5772d85e418d9
minWachya <61674991+minWachya@users.noreply.github.com>
2025-06-03 17:13:13 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.kt
+++ 프로그래머스/3/43162. 네트워크/네트워크.kt
@@ -2,23 +2,23 @@ class Solution {
     var answer = 0
     
     fun solution(n: Int, computers: Array<IntArray>): Int {
-        val visited = BooleanArray(n) { false }
+        val visited = BooleanArray(n) {false}
         for(i in 0 until n) {
             if(!visited[i]) {
                 answer++
-                dfs(n, computers, visited, i)
+                find(n, computers, visited, i)
             }
-            
         }
         
         return answer
     }
     
-    fun dfs(n: Int, computers: Array<IntArray>, visited: BooleanArray, i: Int) {
+    fun find(n: Int, computers: Array<IntArray>, visited: BooleanArray, i: Int) {
         visited[i] = true
         
         for(j in 0 until n) {
-            if(!visited[j] && computers[i][j] == 1) dfs(n, computers, visited, j)
+            if(!visited[j] && computers[i][j] == 1) find(n, computers, visited, j)
         }
     }
+    
 }
