5efa22df3985d8e0b91842b02921955ffd3d127c
minWachya <61674991+minWachya@users.noreply.github.com>
2025-06-04 14:16:40 +0900

    [level 1] Title: 실패율, Time: 1072.29 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.kt
+++ 프로그래머스/1/42889. 실패율/실패율.kt
@@ -1,19 +1,17 @@
+data class Stage(val level: Int, val failRate: Double)
+
 class Solution {
-    fun solution(N: Int, stages: IntArray): IntArray {
-        var answer = intArrayOf()        
-        val fail = (1..N).map{ stage -> 
-            if(stages.count{ it >= stage } == 0) 0.toDouble()
-            else stages.count{ it == stage }.toDouble() / stages.count{ it >= stage }.toDouble()
-            }
-        val sortFail = fail.toSet().sortedDescending()
+    fun solution(N: Int, stages: IntArray): IntArray {  
+        var answer = arrayOf<Stage>()
         
-        for(i in 0 until sortFail.size) {
-            for(j in 0 until fail.size) {
-                if(sortFail[i] == fail[j]) answer += j+1
+        (1..N).forEach{ level -> 
+            if(stages.count{ it >= level } == 0) {
+                answer += Stage(level, 0.toDouble())
+            }
+            else {
+                answer += Stage(level, stages.count{ it == level }.toDouble() / stages.count{ it >= level }.toDouble())
             }
         }
-        
-        
-        return answer
+        return answer.sortedByDescending{ it.failRate }.map{ it.level }.toIntArray()
     }
 }
