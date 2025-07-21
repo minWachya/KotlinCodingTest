83120af38542ac7dc1cb8d025ac8a3d53d91eb7a
minWachya <61674991+minWachya@users.noreply.github.com>
2025-07-21 16:58:42 +0900

    [level 1] Title: 대충 만든 자판, Time: 75.58 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.kt
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.kt
@@ -1,24 +1,9 @@
 class Solution {
-    fun solution(keymap: Array<String>, targets: Array<String>): IntArray {
-        var answer: IntArray = intArrayOf()
-   
-        targets.forEach{ target ->
-            var count = 0
-            var flag = true
-            for(i in 0 until target.length){
-                val orders = keymap
-                    .map{ it.indexOf(target[i]) + 1 }
-                    .filter{ it > 0 }
-                    .sorted()
-                if(orders.size == 0) {
-                    answer += -1
-                    flag = false
-                    break
-                } else count += orders[0]
-            }
-            if(flag) answer += count
-        }
-        
-        return answer
-    }
+    fun solution(keymap: Array<String>, targets: Array<String>): IntArray = 
+    targets.map{ target ->
+        target.map{ c -> keymap.map{ it.indexOf(c) + 1 }
+                .filter{ it > 0 }
+                .let{ list -> if(list.size == 0) -1 else list.minOf{ it } }
+        }.let{ if(-1 in it) -1 else it.sum() }
+    }.toIntArray()
 }
