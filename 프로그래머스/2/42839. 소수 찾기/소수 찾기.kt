d240c1de31dfae18e5751532aea72679023a7321
minWachya <61674991+minWachya@users.noreply.github.com>
2025-07-31 22:11:15 +0900

    [level 2] Title: 소수 찾기, Time: 205.65 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.kt
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.kt
@@ -2,29 +2,25 @@ class Solution {
     private var numberSet = setOf<Int>()
     
     fun solution(numbers: String): Int {
-        combinatorNumber(numbers, "")
+        combine(numbers, "")
         return numberSet.count { checkPrime(it) }
     }
     
-    fun combinatorNumber(numbers: String, result: String) {
-        if (result.isNotEmpty()) numberSet += result.toInt()
-        if (numbers.isNotEmpty()){
+    fun combine(numbers: String, result: String) {
+        if (result.length > 0) numberSet += result.toInt()
+        if (numbers.length > 0){
             numbers.forEachIndexed { i, c ->
-                combinatorNumber(numbers.removeRange(i..i), c.plus(result))
+                combine(numbers.removeRange(i..i), c.plus(result))
             }
         }
     }
     
-    
     fun checkPrime(n: Int): Boolean {
-        when (n) {
-            0 -> return false
-            1 -> return false
-            2 -> return true
-            else ->
-                for (i in 2..(n/2)+1) {
-                    if(n % i == 0) return false
-                }
+        if(n < 2) return false
+        else {
+            for(i in 2..n/2) {
+                if(n % i == 0) return false
+            }
         }
         return true
     }
