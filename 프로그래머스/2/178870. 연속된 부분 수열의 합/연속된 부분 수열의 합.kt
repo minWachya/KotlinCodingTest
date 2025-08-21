c3fb3394612dc7d301caebddafe7140971ef449d
minWachya <61674991+minWachya@users.noreply.github.com>
2025-08-21 17:25:11 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 18.51 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.kt
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.kt
@@ -1,24 +1,26 @@
 class Solution {
     fun solution(sequence: IntArray, k: Int): IntArray {
-        var l = 0
-        var r = 0
-        var sum = sequence[r]
-        var answer = intArrayOf(0,Int.MAX_VALUE)
+        var answer: IntArray = intArrayOf(0, Int.MAX_VALUE)
         
-        while(l < sequence.size){
-            if(sum < k){
-                if(r == sequence.size-1) break
-                sum += sequence[++r]
-            }else{
-                if(sum == k){
-                    if(answer[1]-answer[0] > r-l){
-                        answer[0] = l
-                        answer[1] = r
-                    }
+        var left = 0
+        var right = 0
+        var sum = sequence[0]
+        var max = sequence.size
+        
+        while(left < max && right < max) {
+            if(sum < k) {
+                if(right+1 == max) break
+                sum += sequence[++right]
+            }
+            else {
+                if(sum == k && answer[1] - answer[0] > right - left) {
+                    answer[0] = left
+                    answer[1] = right
                 }
-                sum -= sequence[l++]
+                sum -= sequence[left++]
             }
         }
+        
         return answer
     }
 }
